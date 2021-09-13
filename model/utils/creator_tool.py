dca5b46c52e7aa520729029091d12cc6d9b58d90
PPTMiao <87941025+PPTMiao@users.noreply.github.com>
2021-09-13 15:26:48 +0800

    Create creator_tool.py

--- model/utils/creator_tool.py
+++ model/utils/creator_tool.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torchvision.ops import nms
 from model.utils.bbox_tools import bbox2loc, bbox_iou, loc2bbox
-
+#bbox2loc:计算偏差；loc2bbox:计算
 
 class ProposalTargetCreator(object):
     """Assign ground truth bounding boxes to given RoIs.
